019ae427e1c7dd685c9f2a1d788f5dec0cc4a4f2
Karin Bhandari <karinbhandari7.21@gmail.com>
2020-08-22 15:44:52 +0545

    Cypress removed sd

--- src/utilities/redux.ts
+++ src/utilities/redux.ts
@@ -5,8 +5,10 @@
  * @param {Object} head
  * @param {Array} targets
  */
-const enhanceAll = (head, targets) => targets.reduce((ac, cv) => cv(ac), head)
+const enhanceAll = (head, targets) => targets.reduce((ac, cv) => cv(ac), head);
 
 module.exports = {
   enhanceAll,
-}
+};
+
+export {};
